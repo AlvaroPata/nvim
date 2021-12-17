8ffc21d3955eff4f4afebaedb529be61d07b402c
Álvaro Pata <helmanrm@gmail.com>
2021-12-17 08:55:06 +0100

    translate mappings into lua

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,178 @@
+--   /$$
+--  | $$
+--  | $$   /$$  /$$$$$$  /$$   /$$  /$$$$$$$
+--  | $$  /$$/ /$$__  $$| $$  | $$ /$$_____/
+--  | $$$$$$/ | $$$$$$$$| $$  | $$|  $$$$$$
+--  | $$_  $$ | $$_____/| $$  | $$ \____  $$
+--  | $$ \  $$|  $$$$$$$|  $$$$$$$ /$$$$$$$/
+--  |__/  \__/ \_______/ \____  $$|_______/
+--                       /$$  | $$
+--                      |  $$$$$$/
+--                       \______/
+
+-- remap Y to yank to end of line
+nnoremap('Y', 'y$')
+vnoremap('Y', 'y$')
+
+-- delete text without yanking
+nnoremap('<leader>d', '"_d')
+vnoremap('<leader>d', '"_d')
+
+-- turn off search highlighting
+nnoremap('<leader>hl', ':nohlsearch<cr>')
+
+-- neovim terminal can exit to normal mode with <esc> now
+tnoremap('<esc>', [[<c-\><c-n>]])
+
+-- swap between light and dark themes for rose-pine (Toggle Theme)
+nnoremap('<leader>tt', [[<cmd>lua require('rose-pine.functions').toggle_variant({ 'moon', 'dawn' })<cr>]])
+
+
+-- Better nav for omnicomplete
+-- inoremap('<expr>', '<c-j> ("\<C-n>")')
+-- inoremap('<expr>', '<c-k> ("\<C-p>")')
+
+-- Use alt + hjkl to resize windows
+-- nnoremap('<M-j>', '   :resize -2<CR>')
+-- nnoremap('<M-k>', '   :resize +2<CR>')
+-- nnoremap('<M-h>', '   :vertical resize -2<CR>')
+-- nnoremap('<M-l>', '   :vertical resize +2<CR>')
+
+-- Escape insert mode and add new line below with jkl
+inoremap('JK', '<esc>o')
+
+-- close buffer from normal mode with jk
+nnoremap('JK', ':bd<cr>')
+
+--[[
+" Easy CAPS
+inoremap('<c-u>', '<ESC>viwUi')
+nnoremap('<c-u>', 'viwU<Esc>')
+
+" TAB in general mode will move to text buffer
+nnoremap('<TAB>', ':bnext<CR>')
+" BACKSPACE will go back
+nnoremap('<BACKSPACE>', ':bprevious<CR>')
+--]]
+
+-- save file from normal mode with <leader>s
+nnoremap('<leader>s', ':w<cr>')
+-- Alternate way to save
+nnoremap('<C-s>', ':w<CR>')
+-- Alternate way to quit
+nnoremap('<C-Q>', ':wq!<CR>')
+-- Use control-c instead of escape
+nnoremap('<C-c>', '<Esc>')
+-- <TAB>: completion.
+-- Set in coc plug-config file
+-- inoremap('<expr><TAB>', 'pumvisible() ? "\<C-n>" : "\<TAB>"')
+
+-- Better tabbing
+vnoremap('<', '<gv')
+vnoremap('>', '>gv')
+
+-- Better window navigation
+nnoremap('<C-h>', '<C-w>h')
+nnoremap('<C-j>', '<C-w>j')
+nnoremap('<C-k>', '<C-w>k')
+nnoremap('<C-l>', '<C-w>l')
+
+-- ??????
+nnoremap('<Leader>o', 'o<Esc>^Da')
+nnoremap('<Leader>O', 'O<Esc>^Da')
+
+-- edit .vimrc
+nnoremap('<leader>v', ':vsplit $MYVIMRC<cr>')
+
+-- - moves line up
+nnoremap('<leader>-', 'ddp')
+-- _ moves line down
+nnoremap('<leader>_', 'ddkP')
+
+-- add double quotes around word
+nnoremap('<leader>"', 'viw<esc>a"<esc>bi"<esc>lel')
+-- add single quotes around word
+nnoremap("<leader>'", "viw<esc>a'<esc>bi'<esc>lel")
+-- add single quotes around visually selected word
+vnoremap("<leader>'", "c''<esc>hpl")
+-- add double quotes around visually selected word
+vnoremap('<leader>"', 'c""<esc>hpl')
+
+-- Comment
+-- nnoremap('<leader>/', ':Commentary<CR>')
+-- vnoremap('<leader>/', ':Commentary<CR>')
+
+-- go to the BOL
+nnoremap('<leader>H', '^')
+-- go to EOL
+nnoremap('<leader>L', '$')
+
+-- Clipboard
+nnoremap('<Leader>y', '"*y')
+nnoremap('<Leader>p', '"*p')
+nnoremap('<Leader>Y', '"+y')
+nnoremap('<Leader>P', '"+p')
+
+-- nerdtree mappings
+nnoremap('<leader>n', ':NERDTreeFocus<CR>')
+nnoremap('<C-n>', ':NERDTree<CR>')
+nnoremap('<C-t>', ':NERDTreeToggle<CR>')
+-- nnoremap('<C-f>', ':NERDTreeFind<CR>')
+
+-- operator-pending mappings
+-- inside function [b]ody
+onoremap('b', '/return<cr>')
+-- inside [p]arenthesis
+onoremap('p', 'i(')
+-- inside next parenthesis
+onoremap('in(', ':<c-u>normal! f(vi(<cr>')
+-- inside last parenthesis
+onoremap('il(', ':<c-u>normal! F)vi(<cr>')
+-- inside next square brackets
+onoremap('in[', ':<c-u>normal! f[vi[<cr>')
+-- inside last square brackets
+onoremap('il[', ':<c-u>normal! F]vi[<cr>')
+-- inside next curly brackets
+onoremap('in{', ':<c-u>normal! f{vi{<cr>')
+-- inside last curly brackets
+onoremap('il{', ':<c-u>normal! F}vi{<cr>')
+-- inside next single quotes
+onoremap("in'", ":<c-u>normal! f'vi'<cr>")
+-- inside last single quotes
+onoremap("il'", ":<c-u>normal! F'vi'<cr>")
+-- inside next double quotes
+onoremap('in"', ':<c-u>normal! f"vi"<cr>')
+-- inside last double quotes
+onoremap('il"', ':<c-u>normal! F"vi"<cr>')
+-- around next parenthesis
+onoremap('an(', ':<c-u>normal! f(va(<cr>')
+-- around last parenthesis
+onoremap('al(', ':<c-u>normal! F)va(<cr>')
+-- around next square brackets
+onoremap('an[', ':<c-u>normal! f[va[<cr>')
+-- around last square brackets
+onoremap('al[', ':<c-u>normal! F]va[<cr>')
+-- around next curly brackets
+onoremap('an{', ':<c-u>normal! f{va{<cr>')
+-- around last curly brackets
+onoremap('al{', ':<c-u>normal! F}va{<cr>')
+-- around next single quotes
+onoremap("an'", ":<c-u>normal! f'va'<cr>")
+-- around last single quotes
+onoremap("al'", ":<c-u>normal! F'va'<cr>")
+-- around next double quotes
+onoremap('an"', ':<c-u>normal! f"va"<cr>')
+-- around last double quotes
+onoremap('al"', ':<c-u>normal! F"va"<cr>')
+
+-- start Goyo
+nnoremap('<Leader>yo', ':Goyo 90<bar> highlight StatusLineNC ctermfg=blue <cr>')
+
+-- FZF keymaps
+nnoremap('<leader>k', ':Files<CR>')
+nnoremap('<leader>h', ':History:<CR>')
+nnoremap('<leader>b', ':Buffers<CR>')
+nnoremap('<leader>g', ':Rg<CR>')
+nnoremap('<leader>m', ':Marks<CR>')
+
+
