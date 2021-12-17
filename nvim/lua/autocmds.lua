d17821160763d8a61c3331273ecc3145cc6be5e5
Álvaro Pata <helmanrm@gmail.com>
2021-12-17 09:00:38 +0100

    copy vimscript autocmmands over, not yet translated

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -2,3 +2,21 @@ local cmd = vim.cmd
 
 -- highlight yank for a brief second for visual feedback
 cmd 'autocmd! TextYankPost * lua vim.highlight.on_yank { on_visual = false }'
+
+--[[
+augroup filetype python
+    autocmd!
+    " go to imports"
+    autocmd Filetype python nnoremap <buffer> <leader>i gg}O
+augroup END
+
+augroup filetype_html
+    autocmd!
+    autocmd FileType html nnoremap <buffer> <localleader>f Vatzf
+augroup END
+
+augroup requirents_version
+    autocmd!
+    autocmd bufread,bufnewfile requirements.txt let @r='f=r>wyWA,<pdiwxw'
+augroup END
+]]--
