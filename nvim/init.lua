bf695511ebbea62ccb8be54f13a12f5e25f1e8d4
Álvaro Pata <helmanrm@gmail.com>
2021-12-17 06:59:49 +0100

    add lua configurations and init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+-- autoinstall packer.nvim if not already installed
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  vim.cmd 'packadd packer.nvim'
+end
+
+-- make sure that globals.lua is required first, as we want to use the
+-- functions and helpers we add there EVERYWHERE in our configuration
+require 'globals'
+
+-- everything else here, the order *shouldn't* matter as much I prefer to put
+-- plugins as far towards the end of my require statements so that if you
+-- introduce a bug on accident, its likely that the rest of your config works
+-- fine other than some plugin configuration that is going awry
+require 'settings'
+require 'plugins'
+require 'mappings'
+require 'autocmds'
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,64 @@
+-- aliases for Lua API functions we will use a lot!
+local opt = vim.opt
+local o = vim.o
+
+-- settings
+vim.cmd [[
+syntax on
+filetype plugin indent on
+]]
+vim.g.mapleader = ' '
+vim.g.vimsyn_embed = 'lPr'
+
+
+o.autoindent = true
+o.autoread = true
+o.clipboard = 'unnamedplus'
+o.cmdheight = 2
+o.colorcolumn = '80'
+o.completeopt = 'menu,menuone,noinsert'
+o.cursorline = true
+o.encoding = 'utf-8'
+o.errorbells = false
+o.expandtab = true
+o.fileencoding = 'utf-8'
+o.foldmethod = 'manual'
+o.hidden = true
+o.ignorecase = true
+o.inccommand = 'split'
+o.incsearch = true
+o.laststatus = 2
+o.lazyredraw = true
+o.mouse = 'n'
+o.number = true
+o.path = '.,**'
+o.relativenumber = true
+o.scrolloff = 8
+o.shiftwidth=2
+o.showmode = false
+o.showtabline = 1
+o.signcolumn = 'yes'
+o.smartcase = true
+o.smartindent = true
+o.softtabstop=2
+o.splitbelow = true
+o.splitright = true
+o.swapfile = false
+o.tabstop=2
+o.termguicolors = true
+o.timeoutlen = 500
+o.updatetime = 300
+o.wildignore = '*/node_modules/*,*/.git/*,DS_Store,*/venv/*,*/__pycache__/*,*.pyc'
+o.wildmenu = true
+o.wildmode = 'full'
+o.wildoptions = 'pum'
+o.wrap = false
+o.writebackup = false
+opt.formatoptions:append('jnrql')
+opt.formatoptions:remove('2tac')
+opt.iskeyword:append('-')
+opt.shortmess:append('c')
+
+-- colorscheme
+vim.g.rose_pine_variant='moon'
+vim.cmd 'colorscheme rose-pine'
