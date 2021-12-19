e4b50546eb6f4572ada9ddc300e6ad532ea5708f
Álvaro Pata <helmanrm@gmail.com>
2021-12-19 13:28:15 +0100

    Fix sourcing of plugins renamed plugin configuration folder to match convention from vimconf.

--- nvim/lua/plug_configs/init.lua
+++
@@ -1,8 +0,0 @@
-local plugin_config_root = 'plug_configs.'
-local plugin_config_files = vim.fn.globpath('lua/plug_configs', '*.lua')
-
-for plugin_name in plugin_config_files:gmatch('([%w%d-_]+).lua') do
-  if plugin_name ~= 'init' then
-    require(plugin_config_root .. plugin_name)
-  end
-end
