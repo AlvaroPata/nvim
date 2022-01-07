dd1c77ee3d57f9656f5888388f116de3b5c83d85
Álvaro Pata <helmanrm@gmail.com>
2022-01-07 21:21:13 +0100

    Set Filename to display relative path

--- nvim/lua/plugin_configs/lualine.lua
+++ nvim/lua/plugin_configs/lualine.lua
@@ -10,15 +10,15 @@ require('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', { 'diagnostics', sources = { 'nvim_diagnostic' } } },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {},
