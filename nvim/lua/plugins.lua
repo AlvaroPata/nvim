5234ebe1d536942fd128442f2c2584bb0879b963
Álvaro Pata <helmanrm@gmail.com>
2022-05-10 16:16:20 +0200

    add rest plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -11,6 +11,9 @@ return require('packer').startup(function(use)
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
 
+  -- ejs
+  use 'nikvdp/ejs-syntax'
+
   -- git
   use { 'tpope/vim-fugitive' }
   use { 'tpope/vim-rhubarb' }
@@ -134,4 +137,35 @@ return require('packer').startup(function(use)
   use 'vimwiki/vimwiki'
   use 'mattn/emmet-vim'
   use 'rhysd/vim-grammarous'
+
+  use {
+    'NTBBloodbath/rest.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('rest-nvim').setup {
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          show_http_info = true,
+          show_headers = true,
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      }
+    end,
+  }
 end)
